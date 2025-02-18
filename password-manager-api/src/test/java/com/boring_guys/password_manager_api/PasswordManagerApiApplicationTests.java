9c05cafe1c6a7b1e5a79dae7849c2ba5dcd40603
AaryTrivedi <aarytrivedi@gmail.com>
2025-02-18 15:33:39 -0500

    Initializes API using spring initializr

---
+++ password-manager-api/src/test/java/com/boring_guys/password_manager_api/PasswordManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boring_guys.password_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
